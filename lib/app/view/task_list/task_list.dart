ee50ab25f2358d34a7b1bb9c047e34b0b24a9c77
Javier Barrera <js.barrerat1@uniandes.edu.co>
2025-03-24 11:25:00 -0500

    Add state to ListTaskPage

--- lib/app/view/task_list/task_list.dart
+++ lib/app/view/task_list/task_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todos_app/app/view/components/h1_title.dart';
-// import 'package:todos_app/app/view/components/onboarding.dart';
+import 'package:todos_app/app/view/components/shape.dart';
 import 'package:todos_app/app/view/model/task.dart';
 
 class TaskListPage extends StatelessWidget {
@@ -11,22 +11,8 @@ class TaskListPage extends StatelessWidget {
     return Scaffold(
       // appBar: AppBar(title: Center(child: Text("Lista de tareas"))),
       body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(left: 30, right: 30, top: 35),
-            child: H1Title("Tareas", ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-              child: ListView.separated(
-                itemBuilder: (_, index) => _TaskItem(taskList[index]),
-                separatorBuilder: (_, __) => SizedBox(height: 10),
-                itemCount: taskList.length,
-              ),
-            ),
-          ),
-        ],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [const _Header(), Expanded(child: const _TaskList())],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -39,33 +25,108 @@ class TaskListPage extends StatelessWidget {
 }
 
 class _TaskItem extends StatelessWidget {
-  const _TaskItem(this.task);
+  const _TaskItem(this.task, {this.onTap});
 
   final Task task;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(5),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 21, vertical: 18),
-        child: Row(
-          children: [
-            Icon(Icons.check_box_outline_blank, color: Colors.black),
-            SizedBox(width: 10),
-            Text(task.title, style: TextStyle(fontSize: 16, color: Colors.black)),
-          ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 21, vertical: 18),
+          child: Row(
+            children: [
+              task.done
+                  ? Icon(Icons.check_box, color: Colors.black)
+                  : Icon(Icons.check_box_outline_blank, color: Colors.black),
+              SizedBox(width: 10),
+              Text(
+                task.title,
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-final taskList = <Task>[
-  Task("Estudiar para algo"),
-  Task("Hacer algo"),
-  Task("Comprar algo"),
-  Task("Hacer algo más"),
-];
+class _Header extends StatelessWidget {
+  const _Header();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Theme.of(context).colorScheme.primary,
+      child: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Row(children: const [Shape()]),
+          Column(
+            children: [
+              const SizedBox(height: 100),
+              Image.asset(
+                'assets/images/tasks-list-image.png',
+                width: 120,
+                height: 120,
+              ),
+              const SizedBox(height: 16),
+              const H1Title('Completa tus tareas'),
+              const SizedBox(height: 24),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _TaskList extends StatefulWidget {
+  const _TaskList();
+
+  @override
+  State<_TaskList> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<_TaskList> {
+  final taskList = <Task>[
+    Task("Estudiar para algo"),
+    Task("Hacer algo"),
+    Task("Comprar algo"),
+    Task("Hacer algo más"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const H1Title("Tareas"),
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView.separated(
+              itemBuilder:
+                  (_, index) => _TaskItem(
+                    taskList[index],
+                    onTap: () {
+                      taskList[index].done = !taskList[index].done;
+                      setState(() {});
+                    },
+                  ),
+              separatorBuilder: (_, __) => SizedBox(height: 10),
+              itemCount: taskList.length,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
